3590f8d62e966640d9b31a31cd651c50fc375a8c
Matthew <matthew@urhy.me>
2017-12-19 02:41:41 -0800

    update port from 80 to 3000

--- index.js
+++ index.js
@@ -127,6 +127,6 @@ io.on('connection', function(client) {
 
 });
 
-http.listen(80, function() {
-	console.log('listening on *:80');
+http.listen(3000, function() {
+	console.log('listening on *:3000');
 });
